ca5ba21cb11e560947c969e29dbb0229eeee7d92
CarstenS325 <mobiley.making.money@gmail.com>
2024-08-14 22:56:22 -0500

    Create more-about-operators.py

---
+++ more-about-operators.py
@@ -0,0 +1,4 @@
+adjective = 'different'
+print(adjective)
+print('Python syntax can be super', adjective, ', but we like it', sep='.', end=' / ')
+print('Like', 'this', 'is', 'just', 'a', 'long', 'way', 'of', 'just', 'typing', sep='.', end='!')
