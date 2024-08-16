fb7ce32b6d3809bc2f0682415f23c24b1124223d
CarstenS325 <mobiley.making.money@gmail.com>
2024-08-16 00:01:27 -0500

    Create if-instructions.py

---
+++ if-instructions.py
@@ -0,0 +1,9 @@
+answer_a = input('Do you like to travel? y/n: ')
+if answer_a == 'y':
+    answer_b = input('Me Too!! Do you like Japan? y/n: ')
+    if answer_b == 'y':
+        print('Good! What\'s your favorite part?!')
+    else:
+        print('WHAT?!?!')
+else: 
+    print('Why not?')
