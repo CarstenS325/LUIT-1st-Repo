a3d4f7f9b0a8f8cf92fb62617c070633dfcde083
CarstenS325 <mobiley.making.money@gmail.com>
2024-08-22 11:53:21 -0500

    lists updates

---
+++ iterating_lists.py
@@ -0,0 +1,6 @@
+ran_list = [0, 1, 2, 3, 4, 5]
+for number in ran_list: 
+    print('Number:', number)
+
+for number_index in range(len(ran_list)): 
+    print('Index:', number_index, '| Number:', ran_list[number_index], )
---
+++ list_add.py
@@ -0,0 +1,5 @@
+ran_list = [0, 1, 2, 3, 4, 5]
+ran_list.append(6)
+print(ran_list)
+ran_list.insert(3, 3.5)
+print(ran_list)
